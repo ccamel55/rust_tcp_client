a9cd62b03d9069b6c308e81b5d4be6e0ece09dc2
ccamel55 <allanma31@gmail.com>
2023-09-27 02:25:53 +1300

    add tcp thread

---
+++ src/helpers/config.rs
@@ -0,0 +1,45 @@
+use std::fs;
+use std::path::Path;
+use serde_json::Result;
+use serde::{Deserialize, Serialize};
+
+const CONFIG_NAME: &str = "config.json";
+const CONFIG_DEFAULT: &str = r#"
+{
+    "udp_address": "127.0.0.1",
+    "udp_port": 20002
+}
+"#;
+
+#[derive(Serialize, Deserialize)]
+pub struct ClientConfig
+{
+    pub udp_address: String,
+    pub udp_port: u32,
+}
+
+pub fn load_config() -> Result<ClientConfig> {
+
+    // check if file exists, if not overwrite it
+    if !Path::new(CONFIG_NAME).exists()
+    {
+        println!("config does not exists, creating default file");
+
+        fs::write(CONFIG_NAME, CONFIG_DEFAULT)
+            .expect("TODO: panic message");
+    }
+
+    let contents = fs::read_to_string(CONFIG_NAME);
+
+    if contents.is_err()
+    {
+        println!("could not open config file");
+        assert!(false);
+    }
+
+    let config_as_str = contents.unwrap();
+    println!("found config, raw json: {}", config_as_str);
+
+    // parse into json crate
+    return serde_json::from_str(config_as_str.as_str())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,26 @@
-use std::fs;
-use std::path::Path;
-use serde_json::Result;
-use serde::{Deserialize, Serialize};
+use std::thread;
 
-const CONFIG_NAME: &str = "config.json";
-const CONFIG_DEFAULT: &str = r#"
-{
-    "udp_address": "127.0.0.1",
-    "udp_port": 20002
-}
-"#;
+mod helpers;
 
-#[derive(Serialize, Deserialize)]
-struct ClientConfig
-{
-    udp_address: String,
-    udp_port: u32,
-}
+fn tcp_thread() {
+    loop {
+        // todo: process TCP messages here
 
-fn load_config() -> Result<ClientConfig> {
-
-    // check if file exists, if not overwrite it
-    if !Path::new(CONFIG_NAME).exists()
-    {
-        println!("config does not exists, creating default file");
-
-        fs::write(CONFIG_NAME, CONFIG_DEFAULT)
-            .expect("TODO: panic message");
     }
-
-    let contents = fs::read_to_string(CONFIG_NAME);
-
-    if contents.is_err()
-    {
-        println!("could not open config file");
-        assert!(false);
-    }
-
-    let config_as_str = contents.unwrap();
-    println!("found config, raw json: {}", config_as_str);
-
-    // parse into json crate
-    return serde_json::from_str(config_as_str.as_str())
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let config = load_config().expect("poop");
+    // load config
+    let config = helpers::config::load_config().expect("poop");
     println!("address: {}, port: {}", config.udp_address, config.udp_port);
+
+    // start TCP port, is move needed here??
+    let tcp_thread_handle = thread::spawn(move || tcp_thread());
+
+    // wait until thread has finished before shutting down process
+    tcp_thread_handle.join().unwrap();
+
+    println!("TCP thread closed, shutting down");
 }
